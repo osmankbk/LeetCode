c407b4b14cd7df0880074cc87743c3901e868aa2
osmankbk <32382267+osmankbk@users.noreply.github.com>
2022-05-26 15:40:23 -0400

    Time: 119 ms, Memory: 42.6 MB - LeetHub

--- valid-anagram/valid-anagram.js
+++ valid-anagram/valid-anagram.js
@@ -4,8 +4,11 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let anagram = {};
+    
     if(t.length !== s.length) return false;
+    
+    let anagram = {};
+    
     for(let i = 0; i < s.length; i++) {
       let char = s[i];
       anagram[char] ? anagram[char]++ : anagram[char] = 1;
