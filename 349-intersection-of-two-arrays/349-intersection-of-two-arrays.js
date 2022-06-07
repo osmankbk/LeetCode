b86c6bc5e17013a95dc772a11936f8c05efa5191
osmankbk <32382267+osmankbk@users.noreply.github.com>
2022-06-06 20:38:16 -0400

    Time: 107 ms (17.10%), Space: 42.1 MB (96.59%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -4,18 +4,12 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let hash = {}
    let intersect = [];
-   let rmv_duplicate_nums1 = [...new Set(nums1)];
-   let rmv_duplicate_nums2 = [...new Set(nums2)];
-  
-   for(let i = 0; i < rmv_duplicate_nums1.length; i++) {
-      let num = rmv_duplicate_nums1[i];
-      hash[num] = true;
-   }
+   nums1.forEach(num => {
+      if(nums2.includes(num)) {
+         intersect.push(num);
+      }
+   })
 
-   for(const num of rmv_duplicate_nums2) {
-      if(hash[num]) intersect.push(num);
-   }
-   return intersect;
+   return [...new Set(intersect)];
 };
